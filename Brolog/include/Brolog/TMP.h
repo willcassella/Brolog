726b4cd8f8944440460ebb7001de688c5df9b174
Will Cassella <wcassella@gmail.com>
2016-10-23 21:03:26 -0600

    Added some comments

--- Brolog/include/Brolog/TMP.h
+++ Brolog/include/Brolog/TMP.h
@@ -5,14 +5,13 @@
 
 namespace brolog
 {
+	/* Template meta-programming namespace. */
 	namespace tmp
 	{
 		template <typename ... T>
 		struct type_list {};
 
-		template <typename T>
-		struct type {};
-
+		/* Given a type_list and a type, evaluates to std::true_type if the type exists in the type_list, std::false_type otherwise. */
 		template <typename TypeList, typename T>
 		struct element_of;
 
@@ -31,9 +30,11 @@ namespace brolog
 		{
 		};
 
+		/* Similar to 'type_list', but for character constants (here used for variable names). */
 		template <char ... Cs>
 		using char_list = std::integer_sequence<char, Cs...>;
 
+		/* Given a character and a character list, evaluates to std::true_type if the given character exists in the char_list, std::false_type otherwise. */
 		template <char T, typename CharList>
 		struct element_of_char_list;
 
@@ -52,6 +53,7 @@ namespace brolog
 		{
 		};
 
+		/* Replacement for C++17 fold expressions, 'or'. */
 		template <bool B, bool ... Bs>
 		struct fold_or : std::bool_constant<B || fold_or<Bs...>::value>
 		{
@@ -62,11 +64,13 @@ namespace brolog
 		{
 		};
 
+		/* Evaluates to std::true_type if the given base type is the base of any of the given types, std::false_type otherwise. */
 		template <typename BaseT, typename ... DerivedTs>
 		struct is_base_of_any : fold_or<std::is_base_of<BaseT, DerivedTs>::value...>
 		{
 		};
 
+		/* Casts the first element of the series of types that can be cast to the target type. */
 		template <typename Target, typename T, typename ... Ts>
 		auto cast_first_suitable(T& first, Ts& ... rest) -> std::enable_if_t<std::is_base_of<Target, T>::value, Target&>
 		{
